0d50cf2671c2ea1814f0f7e6b09aa851eb0b2f76
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2024-03-09 18:58:38 -0800

    Set up target number and stuff and things

--- Card Game/Assets/Scripts/CardDisplay.cs
+++ Card Game/Assets/Scripts/CardDisplay.cs
@@ -8,36 +8,67 @@ public class CardDisplay : MonoBehaviour
     public Card card;
     public Canvas cardCanvas;
     public Image cardImage;
+    [SerializeField] GameObject manager;
+
+    public bool letPlayerFlip = false;
 
     // Start is called before the first frame update
     void Start()
     {
         cardImage.sprite = card.faceSprite;
-        StartCoroutine(flipCard());
     }
 
-    private void Update()
+    private void OnMouseDown()
     {
-        
+        if (letPlayerFlip) {flip();}
     }
+
+    public void flip()
+    {
+        StartCoroutine(flipCard());
+        manager.GetComponent<GameManager>().playerGuess += card.value;
+        Debug.Log("Player guess: " + manager.GetComponent<GameManager>().playerGuess);
+    }
+
     public IEnumerator flipCard()
     {
-        while (cardCanvas.transform.rotation.eulerAngles.y < 90)
+        float yRotation = cardImage.transform.rotation.eulerAngles.y;
+        if (yRotation == 0)
         {
-            cardCanvas.transform.Rotate(0, 1f, 0);
-            yield return new WaitForSeconds(0.001f);
-        }
-        if (cardImage.sprite == card.faceSprite)
-        {
+            // Card is face up
+            
+            while (yRotation < 90)
+            {
+                yRotation += 1f;
+                cardImage.transform.Rotate(0, 1f, 0);
+                yield return new WaitForSeconds(0.001f);
+            }
             cardImage.sprite = card.backSprite;
+            while (yRotation < 180)
+            {
+                yRotation += 1f;
+                cardImage.transform.Rotate(0, 1f, 0);
+                yield return new WaitForSeconds(0.001f);
+            }
+            
         } else
         {
+            // Card is flipped
+
+            while (yRotation > 90)
+            {
+                yRotation -= 1f;
+                cardImage.transform.Rotate(0, -1f, 0);
+                yield return new WaitForSeconds(0.001f);
+            }
             cardImage.sprite = card.faceSprite;
-        }
-        while (cardCanvas.transform.rotation.eulerAngles.y < 180)
-        {
-            cardCanvas.transform.Rotate(0, 1f, 0);
-            yield return new WaitForSeconds(0.001f);
+            while (yRotation > 0)
+            {
+                yRotation -= 1f;
+                cardImage.transform.Rotate(0, -1f, 0);
+                yield return new WaitForSeconds(0.001f);
+                // TODO: fix card flip
+            }
         }
     }
 }
--- Card Game/Assets/Scripts/GameManager.cs
+++ Card Game/Assets/Scripts/GameManager.cs
@@ -1,24 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject cardPrefab;
     public List<Card> deck = new List<Card>();
+    private List<Card> hand = new List<Card>();
+    private List<GameObject> handObjects = new List<GameObject>();
     public Transform[] cardSpawns;
+    [SerializeField] private TMP_Text targetText;
 
     private GameObject card;
+    private int target = 0;
+    private int targetCardCount;
+    public int playerGuess = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        card = Instantiate(cardPrefab, cardSpawns[0].transform.position, cardSpawns[0].transform.rotation);
-        card.GetComponent<CardDisplay>().card = deck[0];
+        for (int i = 0; i < cardSpawns.Length; i++)
+        {
+            int rand = Random.Range(0, deck.Count);
+            card = Instantiate(cardPrefab, cardSpawns[i].transform.position, cardSpawns[0].transform.rotation);
+            card.GetComponent<CardDisplay>().card = deck[rand];
+            deck.RemoveAt(rand);
+            hand.Add(card.GetComponent<CardDisplay>().card);
+            handObjects.Add(card);
+        }
+        SetTarget();
+        Invoke("flipAllCards", 3);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetTarget()
+    {
+        target = 0;
+        targetCardCount = Random.Range(1, hand.Count);
+        List<Card> handCopy = new List<Card>(hand);
+        foreach (Card card in hand)
+        {
+            if (targetCardCount >= 1)
+            {
+                int rand = Random.Range(1, handCopy.Count);
+                // 1/2 chance to add 11 instead of 1 for aces
+                if (handCopy[rand].value == 1 && Random.Range(0, 1) == 1)
+                {
+                    target += 11;
+                } else
+                {
+                    target += handCopy[rand].value;
+                }
+                Debug.Log(handCopy[rand].value);
+                handCopy.RemoveAt(rand);
+                targetCardCount--;
+            }
+        }
+        targetText.text = target.ToString();
+    }
+
+    private void flipAllCards()
+    {
+        foreach (GameObject obj in handObjects)
+        {
+            obj.GetComponent<CardDisplay>().flip();
+            obj.GetComponent<CardDisplay>().letPlayerFlip = true;
+        }
     }
 }
